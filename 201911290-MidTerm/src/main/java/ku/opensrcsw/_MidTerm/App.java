392dea8180b6f2149b00714ad1cbf5ef3eec9d73
Ji-eunChoi <55906783+Ji-eunChoi@users.noreply.github.com>
2020-04-29 10:01:31 +0900

    Add files via upload
    Task 1

---
+++ 201911290-MidTerm/src/main/java/ku/opensrcsw/_MidTerm/App.java
@@ -0,0 +1,15 @@
+package ku.opensrcsw._MidTerm;
+import java.util.Scanner;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	String str;
+    	
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter the keyword you are looking for: ");
+        str = sc.next();
+    }
+}
